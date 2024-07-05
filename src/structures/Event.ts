934b2d29f30928d647fbc4560efff4f025655d2e
Tweenty <tweenty.pro@gmail.com>
2024-07-05 18:08:26 +0200

    add: type event

--- src/structures/Event.ts
+++ src/structures/Event.ts
@@ -1,10 +1,12 @@
 import { type Awaitable, type ClientEvents } from "discord.js";
 import { type StelliaClient } from "@client/index.js";
 
-export interface EventStructure {
+type EventArguments<Event extends keyof ClientEvents> = ClientEvents[Event];
+
+export interface EventStructure<Event extends keyof ClientEvents = keyof ClientEvents> {
     data: {
         name: keyof ClientEvents;
         once: boolean;
     }
-    execute(client: StelliaClient, ...args: unknown[]): Awaitable<unknown>;
+    execute(client: StelliaClient, ...args: EventArguments<Event>): Awaitable<unknown>;
 }
