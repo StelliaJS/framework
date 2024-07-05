1984c9257f51f2c996528c70ad49dfa7a67f7ada
Tweenty <tweenty.pro@gmail.com>
2024-07-05 18:08:39 +0200

    add: interaction handler

--- src/client/StelliaUtils.ts
+++ src/client/StelliaUtils.ts
@@ -21,12 +21,22 @@ import {
     type ModalStructure,
     type SelectMenuStructure
 } from "@structures/index.js";
+import { AnyInteraction, InteractionType } from "@typescript/index.js";
 
 export class StelliaUtils {
     public readonly client: StelliaClient;
+    private interactionHandlers: Map<InteractionType, (interaction: AnyInteraction) => Promise<void>>;
 
     constructor(client: StelliaClient) {
         this.client = client;
+        this.interactionHandlers = new Map([
+            [InteractionType.Autocomplete, this.handleAutoCompleteInteraction],
+            [InteractionType.Button, this.handleButtonInteraction],
+            [InteractionType.ChatInputCommand, this.handleCommandInteraction],
+            [InteractionType.ContextMenuCommand, this.handleContextMenuInteraction],
+            [InteractionType.ModalSubmit, this.handleModalInteraction],
+            [InteractionType.SelectMenu, this.handleSelectMenuInteraction]
+        ]);
     }
 
     public initializeCommands = async (): Promise<void> => {
@@ -45,71 +55,136 @@ export class StelliaUtils {
         }
     }
 
-    public handleAutoCompleteInteraction = async (interaction: AutocompleteInteraction<"cached">, interactionData: AutoCompleteStructure): Promise<void> => {
+    public handleInteraction = async (interaction: AnyInteraction): Promise<void> => {
+        if (interaction.inCachedGuild()) {
+            const interactionType = this.getInteractionType(interaction);
+            if (interactionType === InteractionType.Unknown) {
+                throw new Error("Unknown interaction type");
+            }
+            const handler = this.interactionHandlers.get(interactionType);
+            if (handler) {
+                await handler(interaction);
+            }
+        }
+    }
+
+    private handleAutoCompleteInteraction = async (interaction: AnyInteraction): Promise<void> => {
         try {
-            await interactionData.execute(this.client, interaction);
+            const interactionAutoComplete = interaction as AutocompleteInteraction<"cached">;
+            const autoComplete = this.client.autoCompletes.get<AutoCompleteStructure>(interactionAutoComplete.commandName);
+            if (!autoComplete) return;
+
+            await autoComplete.execute(this.client, interactionAutoComplete);
         } catch (error) {
             console.error(error);
         }
     }
 
-    public handleButtonInteraction = async (interaction: ButtonInteraction<"cached">, interactionData: ButtonStructure): Promise<void> => {
+    private handleButtonInteraction = async (interaction: AnyInteraction): Promise<void> => {
         try {
-            await interactionData.execute(this.client, interaction);
+            const buttonInteraction = interaction as ButtonInteraction<"cached">;
+            const button = this.client.buttons.get<ButtonStructure>(buttonInteraction.customId);
+            if (!button) return;
+
+            await button.execute(this.client, buttonInteraction);
         } catch (error) {
             console.error(error);
         }
     }
 
-    public handleCommandInteraction = async (interaction: ChatInputCommandInteraction<"cached">, interactionData: CommandStructure): Promise<void> => {
+    private handleCommandInteraction = async (interaction: AnyInteraction): Promise<void> => {
         try {
-            await interactionData.execute(this.client, interaction);
+            const interactionCommand = interaction as ChatInputCommandInteraction<"cached">;
+            const command = this.client.commands.get<CommandStructure>(interactionCommand.commandName);
+            if (!command) return;
+
+            await command.execute(this.client, interactionCommand);
         } catch (error) {
             console.error(error);
         }
     }
 
-    public handleContextMenuInteraction = async (interaction: ContextMenuCommandInteraction<"cached">, interactionData: ContextMenuStructure): Promise<void> => {
+    private handleContextMenuInteraction = async (interaction: AnyInteraction): Promise<void> => {
         try {
-            if (interaction.commandType === ApplicationCommandType.Message) {
-                await this.handleMessageContextMenuInteraction(interaction as MessageContextMenuCommandInteraction<"cached">, interactionData);
-            } else if (interaction.commandType === ApplicationCommandType.User) {
-                await this.handleUserContextMenuInteraction(interaction as UserContextMenuCommandInteraction<"cached">, interactionData);
+            const interactionContextMenu = interaction as ContextMenuCommandInteraction<"cached">;
+            if (interactionContextMenu.commandType === ApplicationCommandType.Message) {
+                const messageInteraction = interaction as MessageContextMenuCommandInteraction<"cached">;
+                await this.handleMessageContextMenuInteraction(messageInteraction);
+            } else if (interactionContextMenu.commandType === ApplicationCommandType.User) {
+                const userInteraction = interaction as UserContextMenuCommandInteraction<"cached">;
+                await this.handleUserContextMenuInteraction(userInteraction);
             }
         } catch (error) {
             console.error(error);
         }
     }
 
-    public handleModalInteraction = async (interaction: ModalSubmitInteraction<"cached">, interactionData: ModalStructure): Promise<void> => {
+    private handleModalInteraction = async (interaction: AnyInteraction): Promise<void> => {
         try {
-            await interactionData.execute(this.client, interaction);
+            const interactionModal = interaction as ModalSubmitInteraction<"cached">;
+            const modal = this.client.modals.get<ModalStructure>(interactionModal.customId);
+            if (!modal) return;
+
+            await modal.execute(this.client, interactionModal);
         } catch (error) {
             console.error(error);
         }
     }
 
-    public handleSelectMenuInteraction = async (interaction: AnySelectMenuInteraction<"cached">, interactionData: SelectMenuStructure): Promise<void> => {
+    private handleSelectMenuInteraction = async (interaction: AnyInteraction): Promise<void> => {
         try {
-            await interactionData.execute(this.client, interaction);
+            const interactionSelectMenu = interaction as AnySelectMenuInteraction<"cached">;
+            const selectMenu = this.client.modals.get<SelectMenuStructure>(interactionSelectMenu.customId);
+            if (!selectMenu) return;
+
+            await selectMenu.execute(this.client, interactionSelectMenu);
         } catch (error) {
             console.error(error);
         }
     }
 
-    private handleMessageContextMenuInteraction = async (interaction: MessageContextMenuCommandInteraction<"cached">, interactionData: ContextMenuStructure): Promise<void> => {
+    private handleMessageContextMenuInteraction = async (interaction: MessageContextMenuCommandInteraction<"cached">): Promise<void> => {
         try {
-            await interactionData.execute(this.client, interaction);
+            const messageContextMenu = this.client.contextMenus.get<ContextMenuStructure>(interaction.commandName);
+            if (!messageContextMenu) return;
+
+            await messageContextMenu.execute(this.client, interaction);
         } catch (error) {
             console.error(error);
         }
     }
 
-    private handleUserContextMenuInteraction = async (interaction: UserContextMenuCommandInteraction<"cached">, interactionData: ContextMenuStructure): Promise<void> => {
+    private handleUserContextMenuInteraction = async (interaction: UserContextMenuCommandInteraction<"cached">): Promise<void> => {
         try {
-            await interactionData.execute(this.client, interaction);
+            const userContextMenu = this.client.contextMenus.get<ContextMenuStructure>(interaction.commandName);
+            if (!userContextMenu) return;
+
+            await userContextMenu.execute(this.client, interaction);
         } catch (error) {
             console.error(error);
         }
+    }
+
+    private getInteractionType(interaction: AnyInteraction): InteractionType {
+        if (interaction.isAutocomplete()) {
+            return InteractionType.Autocomplete;
+        }
+        if (interaction.isButton()) {
+            return InteractionType.Button;
+        }
+        if (interaction.isChatInputCommand()) {
+            return InteractionType.ChatInputCommand;
+        }
+        if (interaction.isContextMenuCommand()) {
+            return InteractionType.ContextMenuCommand;
+        }
+        if (interaction.isModalSubmit()) {
+            return InteractionType.ModalSubmit;
+        }
+        if (interaction.isAnySelectMenu()) {
+            return InteractionType.SelectMenu;
+        }
+
+        return InteractionType.Unknown;
     }
 }
--- src/typescript/types.ts
+++ src/typescript/types.ts
@@ -1,1 +1,29 @@
+import {
+    type AnySelectMenuInteraction,
+    type AutocompleteInteraction,
+    type ButtonInteraction,
+    type ChatInputCommandInteraction,
+    type ContextMenuCommandInteraction,
+    type ModalSubmitInteraction,
+    type UserContextMenuCommandInteraction
+} from "discord.js";
+
 export type CustomId = string;
+
+export type AnyInteraction = AutocompleteInteraction<"cached">
+    | ButtonInteraction<"cached">
+    | ChatInputCommandInteraction<"cached">
+    | ContextMenuCommandInteraction<"cached">
+    | ModalSubmitInteraction<"cached">
+    | AnySelectMenuInteraction<"cached">
+    | UserContextMenuCommandInteraction<"cached">;
+
+export enum InteractionType {
+    Autocomplete = "Autocomplete",
+    Button = "Button",
+    ChatInputCommand = "ChatInputCommand",
+    ContextMenuCommand = "ContextMenuCommand",
+    ModalSubmit = "ModalSubmit",
+    SelectMenu = "SelectMenu",
+    Unknown = "Unknown"
+}
