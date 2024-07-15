ab6de85bdc1892e813e33e6cfb215e2139aa314b
Tweenty <tweenty.pro@gmail.com>
2024-07-15 22:01:50 +0200

    add: Managers interface

--- src/typescript/types.ts
+++ src/typescript/types.ts
@@ -7,6 +7,15 @@ import {
     type ModalSubmitInteraction,
     type UserContextMenuCommandInteraction
 } from "discord.js";
+import {
+    type AutoCompleteManager,
+    type ButtonManager,
+    type CommandManager,
+    type ContextMenuManager,
+    type EventManager,
+    type ModalManager,
+    type SelectMenuManager
+} from "@managers/index.js";
 
 export type CustomId = string;
 
@@ -28,17 +37,14 @@ export enum InteractionType {
     Unknown = "Unknown"
 }
 
-export type InteractionData = {
-    commandName: string;
-    commandOptions?: CommandOptions;
-    autoCompletes?: AutoCompletes;
-    buttons?: Buttons;
-    modals?: Modals;
-    selectMenus?: SelectMenus;
-}
-
-type CommandOptions = {
-    [key: string]: string;
+export interface Managers {
+    autoCompletes?: AutoCompleteManager;
+    buttons?: ButtonManager;
+    commands?: CommandManager;
+    contextMenus?: ContextMenuManager;
+    events?: EventManager;
+    selectMenus?: SelectMenuManager;
+    modals?: ModalManager;
 }
 
 type AutoCompletes = {
