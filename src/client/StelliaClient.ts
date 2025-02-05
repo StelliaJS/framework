a72d13f002c4c67360d74814f75b086d0fe26a7f
Tweenty <tweenty.pro@gmail.com>
2025-02-05 23:14:01 +0100

    feat(environments): getEnvironment()

--- src/client/StelliaClient.ts
+++ src/client/StelliaClient.ts
@@ -10,43 +10,58 @@ import {
     SelectMenuManager
 } from "@managers/index.js";
 import { StelliaUtils } from "@client/index.js";
-import { type AnyInteraction, type Manager, type Managers } from "@typescript/index.js";
+import {
+    type AnyInteraction,
+    type Environment,
+    type EnvironmentConfiguration,
+    type Manager,
+    type Managers
+} from "@typescript/index.js";
+import path from "path";
+import * as fs from "node:fs";
+import { pathToFileURL } from 'url';
 
 export class StelliaClient<Ready extends boolean = boolean> extends Client<Ready> {
     private readonly utils: StelliaUtils;
     public readonly managers: Managers = {};
+    public readonly environment: Environment;
 
     public constructor(clientOptions: ClientOptions, stelliaOptions?: StelliaOptions) {
         super(clientOptions);
+
+        if (stelliaOptions?.managers.autoCompletes?.directoryPath) {
+            this.managers.autoCompletes = new AutoCompleteManager(this, stelliaOptions.managers.autoCompletes.directoryPath);
+        }
+
+        if (stelliaOptions?.managers.buttons?.directoryPath) {
+            this.managers.buttons = new ButtonManager(this, stelliaOptions.managers.buttons.directoryPath);
+        }
+
+        if (stelliaOptions?.managers.commands?.directoryPath) {
+            this.managers.commands = new CommandManager(this, stelliaOptions.managers.commands.directoryPath);
+        }
+
+        if (stelliaOptions?.managers.contextMenus?.directoryPath) {
+            this.managers.contextMenus = new ContextMenuManager(this, stelliaOptions.managers.contextMenus.directoryPath);
+        }
+
+        if (stelliaOptions?.managers.events?.directoryPath) {
+            this.managers.events = new EventManager(this, stelliaOptions.managers.events.directoryPath);
+        }
+
+        if (stelliaOptions?.managers.selectMenus?.directoryPath) {
+            this.managers.selectMenus = new SelectMenuManager(this, stelliaOptions.managers.selectMenus.directoryPath);
+        }
+
+        if (stelliaOptions?.managers.modals?.directoryPath) {
+            this.managers.modals = new ModalManager(this, stelliaOptions.managers.modals.directoryPath);
+        }
+
+        if (stelliaOptions?.environment) {
+            this.environment = stelliaOptions.environment;
+        }
+
         this.utils = new StelliaUtils(this);
-
-        if (stelliaOptions?.autoCompletes?.directoryPath) {
-            this.managers.autoCompletes = new AutoCompleteManager(this, stelliaOptions.autoCompletes.directoryPath);
-        }
-
-        if (stelliaOptions?.buttons?.directoryPath) {
-            this.managers.buttons = new ButtonManager(this, stelliaOptions.buttons.directoryPath);
-        }
-
-        if (stelliaOptions?.commands?.directoryPath) {
-            this.managers.commands = new CommandManager(this, stelliaOptions.commands.directoryPath);
-        }
-
-        if (stelliaOptions?.contextMenus?.directoryPath) {
-            this.managers.contextMenus = new ContextMenuManager(this, stelliaOptions.contextMenus.directoryPath);
-        }
-
-        if (stelliaOptions?.events?.directoryPath) {
-            this.managers.events = new EventManager(this, stelliaOptions.events.directoryPath);
-        }
-
-        if (stelliaOptions?.selectMenus?.directoryPath) {
-            this.managers.selectMenus = new SelectMenuManager(this, stelliaOptions.selectMenus.directoryPath);
-        }
-
-        if (stelliaOptions?.modals?.directoryPath) {
-            this.managers.modals = new ModalManager(this, stelliaOptions.modals.directoryPath);
-        }
 
         process.on("unhandledRejection", (error: string) => {
             console.error(`Unhandled promise rejection: ${error}`);
@@ -71,6 +86,37 @@ export class StelliaClient<Ready extends boolean = boolean> extends Client<Ready
         await this.utils.initializeCommands();
     }
 
+    public getEnvironment = async <CustomEnvironment extends EnvironmentConfiguration>(): Promise<CustomEnvironment> => {
+        const chosenEnvironment = process.argv.find(arg => arg.startsWith("--config"))?.split("=")[1];
+        if (!chosenEnvironment) {
+            throw new Error("Environment not provided");
+        }
+
+        const srcPath = path.dirname(path.resolve(process.argv[1]));
+        const filePath = path.join(srcPath, "..", "stellia.json");
+        return new Promise((resolve, reject) => {
+            fs.readFile(filePath, async (err, data) => {
+                if (err) {
+                    return reject(err);
+                }
+
+                try {
+                    const environments = JSON.parse(data.toString()).environments;
+                    if (!Object.keys(environments).includes(chosenEnvironment)) {
+                        return reject(new Error("Invalid environment"));
+                    }
+
+                    const environmentPath = environments[chosenEnvironment];
+                    const environmentAbsolutePath = pathToFileURL(path.join(srcPath, "..", environmentPath.file)).href
+                    const environmentFile = await import(environmentAbsolutePath);
+                    resolve(environmentFile.environment);
+                } catch (error) {
+                    reject(error);
+                }
+            });
+        });
+    }
+
     public handleInteraction = async (interaction: AnyInteraction): Promise<void> => {
         await this.utils.handleInteraction(interaction);
     }
@@ -83,12 +129,15 @@ export class StelliaClient<Ready extends boolean = boolean> extends Client<Ready
 }
 
 interface StelliaOptions {
-    autoCompletes?: ManagerOptions;
-    buttons?: ManagerOptions;
-    commands?: ManagerOptions;
-    contextMenus?: ManagerOptions;
-    events?: ManagerOptions;
-    selectMenus?: ManagerOptions;
-    modals?: ManagerOptions;
+    managers: {
+        autoCompletes?: ManagerOptions;
+        buttons?: ManagerOptions;
+        commands?: ManagerOptions;
+        contextMenus?: ManagerOptions;
+        events?: ManagerOptions;
+        selectMenus?: ManagerOptions;
+        modals?: ManagerOptions;   
+    },
+    environment?: Environment;
 }
 
