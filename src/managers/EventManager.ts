268818948baa10e629a6f924deb6e0d55672dbcc
Tweenty <tweenty.pro@gmail.com>
2025-05-05 00:51:28 +0200

    fix: environment interfaces

--- src/managers/EventManager.ts
+++ src/managers/EventManager.ts
@@ -1,7 +1,11 @@
 import { Collection, Events } from "discord.js";
 import { type StelliaClient } from "@client/index.js";
 import { BaseManager } from "@managers/index.js";
-import { type EventStructure } from "@structures/index.js";
+import {
+    type EventStructure,
+    type EventStructureWithEnvironment,
+    type EventStructureWithoutEnvironment
+} from "@structures/index.js";
 import { type StructureCustomId, type InteractionCustomId } from "@typescript/index.js";
 import { requiredFiles } from "@utils/index.js";
 
@@ -16,12 +20,13 @@ export class EventManager extends BaseManager {
         const events = await requiredFiles<EventStructure>(this.directoryPath);
         this.interactions = events;
 
-        for (const event of this.interactions.values()) {
-            const { name, once } = event.data;
+        for (const eventStructure of this.interactions.values()) {
+            const { name, once } = eventStructure.data;
             if (this.client.environment.areEnvironmentsEnabled) {
                 const environment = await this.client.getEnvironment();
+                const event = eventStructure as EventStructureWithEnvironment;
                 if (name == Events.ClientReady) {
-                    this.client.once(name, () => event.execute(this.client, environment));
+                    this.client.once(Events.ClientReady, () => event.execute(this.client, environment));
                     continue;
                 }
 
@@ -31,6 +36,7 @@ export class EventManager extends BaseManager {
                     this.client.on(name, (...args) => event.execute(this.client, environment, ...args));
                 }
             } else {
+                const event = eventStructure as EventStructureWithoutEnvironment;
                 if (name == Events.ClientReady) {
                     this.client.once(name, () => event.execute(this.client));
                     continue;
