29d5a2b81fa62986c2f8aa665764e5b39b08c435
Tweenty <tweenty.pro@gmail.com>
2026-04-07 16:50:16 +0200

    refactor(translation): improve typing

--- src/utils/translation.ts
+++ src/utils/translation.ts
@@ -1,11 +1,9 @@
 import { type Locale } from "discord.js";
 import { changeLanguage, t } from "i18next";
 
-interface TranslateArgs {
-	[key: string]: any;
-}
+type TranslateArgs = Record<string, unknown>;
 
-export const translateToLocale = async (locale: Locale, key: string, args?: TranslateArgs): Promise<any> => {
+export const translateToLocale = async <T = unknown>(locale: Locale, key: string, args?: TranslateArgs): Promise<T> => {
 	await changeLanguage(locale);
-	return t(key, { interpolation: { escapeValue: false }, returnObjects: true, ...args });
+	return t(key, { interpolation: { escapeValue: false }, returnObjects: true, ...args }) as T;
 };
