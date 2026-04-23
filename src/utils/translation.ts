3db716596cbe3b36ae61f341b4a020f7c516d5fc
Tweentyy <tweenty.pro@gmail.com>
2026-04-23 11:54:06 +0200

    fix(translation): spread operator before all args

--- src/utils/translation.ts
+++ src/utils/translation.ts
@@ -4,5 +4,5 @@ import { t } from "i18next";
 type TranslateArgs = Record<string, unknown>;
 
 export const translateToLocale = <T = unknown>(locale: Locale, key: string, args?: TranslateArgs): T => {
-	return t(key, { lng: locale, interpolation: { escapeValue: false }, returnObjects: true, ...args }) as T;
+	return t(key, { ...args, lng: locale, interpolation: { escapeValue: false }, returnObjects: true }) as T;
 };
