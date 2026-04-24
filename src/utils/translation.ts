35f777b6b7845b200be11a443fe587a5091f8e77
Tweenty <tweenty.pro@gmail.com>
2026-04-24 11:02:45 +0200

    fix(translation): create i18n instance

--- src/utils/translation.ts
+++ src/utils/translation.ts
@@ -1,8 +1,14 @@
-import { t } from "i18next";
+import { i18n, t } from "i18next";
 import { type StelliaLocale } from "@typescript/index.js";
 
 type TranslateArgs = Record<string, unknown>;
 
+let _instance: i18n;
+
+export const initTranslations = (instance: i18n) => {
+	_instance = instance;
+};
+
 export const translateToLocale = <T = unknown>(locale: StelliaLocale, key: string, args?: TranslateArgs): T => {
-	return t(key, { ...args, lng: locale, interpolation: { escapeValue: false }, returnObjects: true }) as T;
+	return _instance.t(key, { ...args, lng: locale, interpolation: { escapeValue: false }, returnObjects: true }) as T;
 };
