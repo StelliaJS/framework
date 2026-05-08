5335320642ebf2d7617dbe75ce2d8ce945c8f691
Tweenty <tweenty.pro@gmail.com>
2026-05-08 12:28:09 +0200

    refactor: rename interactions to specific interaction

--- src/managers/ButtonManager.ts
+++ src/managers/ButtonManager.ts
@@ -3,10 +3,10 @@ import { type StelliaClient } from "@client/index.js";
 import { BaseManager } from "@managers/index.js";
 import { type ButtonStructure } from "@structures/index.js";
 import { type InteractionCustomId, type StructureCustomId } from "@typescript/index.js";
-import { requiredFiles } from "@utils/index.js";
+import { logger, requiredFiles } from "@utils/index.js";
 
 export class ButtonManager extends BaseManager<ButtonStructure> {
-	public interactions: Collection<StructureCustomId, ButtonStructure> = new Collection();
+	public buttons: Collection<StructureCustomId, ButtonStructure> = new Collection();
 
 	private constructor(client: StelliaClient, directoryPath: string) {
 		super(client, directoryPath);
@@ -20,18 +20,19 @@ export class ButtonManager extends BaseManager<ButtonStructure> {
 	}
 
 	public async loadData(): Promise<void> {
-		const buttons = await requiredFiles<ButtonStructure>(this.directoryPath);
-		this.interactions = buttons;
+		this.buttons = await requiredFiles<ButtonStructure>(this.directoryPath);
 		this.setManagerLoaded();
+
+		logger.info(`Loaded ${this.buttons.size} buttons`);
 	}
 
 	public getByCustomId(id: InteractionCustomId): ButtonStructure | null {
-		return this.interactions.get(id) ?? null;
+		return this.buttons.get(id) ?? null;
 	}
 
 	public getByRegex(id: InteractionCustomId): ButtonStructure | null {
 		let button: ButtonStructure | null = null;
-		for (const [customId, action] of this.interactions.entries()) {
+		for (const [customId, action] of this.buttons.entries()) {
 			if (customId instanceof RegExp && customId.test(id)) {
 				button = action;
 				break;
@@ -42,6 +43,6 @@ export class ButtonManager extends BaseManager<ButtonStructure> {
 	}
 
 	public getAll(): Collection<StructureCustomId, ButtonStructure> {
-		return this.interactions;
+		return this.buttons;
 	}
 }
--- src/managers/ContextMenuManager.ts
+++ src/managers/ContextMenuManager.ts
@@ -3,10 +3,10 @@ import { type StelliaClient } from "@client/index.js";
 import { BaseManager } from "@managers/index.js";
 import { type ContextMenuStructure } from "@structures/index.js";
 import { type InteractionCustomId, type StructureCustomId } from "@typescript/index.js";
-import { requiredFiles } from "@utils/index.js";
+import { logger, requiredFiles } from "@utils/index.js";
 
 export class ContextMenuManager extends BaseManager<ContextMenuStructure> {
-	private interactions: Collection<StructureCustomId, ContextMenuStructure> = new Collection();
+	private contextMenus: Collection<StructureCustomId, ContextMenuStructure> = new Collection();
 
 	private constructor(client: StelliaClient, directoryPath: string) {
 		super(client, directoryPath);
@@ -20,18 +20,19 @@ export class ContextMenuManager extends BaseManager<ContextMenuStructure> {
 	}
 
 	public async loadData(): Promise<void> {
-		const contextMenus = await requiredFiles<ContextMenuStructure>(this.directoryPath);
-		this.interactions = contextMenus;
+		this.contextMenus = await requiredFiles<ContextMenuStructure>(this.directoryPath);
 		this.setManagerLoaded();
+
+		logger.info(`Loaded ${this.contextMenus.size} context menus`);
 	}
 
 	public getByCustomId(id: InteractionCustomId): ContextMenuStructure | null {
-		return this.interactions.get(id) ?? null;
+		return this.contextMenus.get(id) ?? null;
 	}
 
 	public getByRegex(id: InteractionCustomId): ContextMenuStructure | null {
 		let contextMenu: ContextMenuStructure | null = null;
-		for (const [customId, action] of this.interactions.entries()) {
+		for (const [customId, action] of this.contextMenus.entries()) {
 			if (customId instanceof RegExp && customId.test(id)) {
 				contextMenu = action;
 				break;
@@ -42,6 +43,6 @@ export class ContextMenuManager extends BaseManager<ContextMenuStructure> {
 	}
 
 	public getAll(): Collection<StructureCustomId, ContextMenuStructure> {
-		return this.interactions;
+		return this.contextMenus;
 	}
 }
