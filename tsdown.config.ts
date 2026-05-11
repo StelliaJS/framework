37c8886b692510fdf5b282598c756011747229c7
Tweenty <tweenty.pro@gmail.com>
2026-05-11 21:42:30 +0200

    refactor: improve tsconfig config

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,16 +1,12 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  entry: "./src/index.ts",
-  platform: "node",
-  dts: true,
-  format: "esm",
-  alias: {
-    "@client": "./src/client",
-    "@constants": "./src/constants",
-    "@managers": "./src/managers",
-    "@structures": "./src/structures",
-    "@typescript": "./src/typescript",
-    "@utils": "./src/utils"
-  }
+	entry: "./src/index.ts",
+	platform: "node",
+	dts: true,
+	format: "esm",
+	clean: true,
+	deps: {
+		skipNodeModulesBundle: true
+	}
 });
