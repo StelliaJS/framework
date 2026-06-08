53e2c68e0c669e907f033d0e8342de44784c1857
Tweenty <tweenty.pro@gmail.com>
2026-06-08 23:01:38 +0200

    feat: select menu paginator component

---
+++ src/structures/paginators/builders/PaginatorSelectMenuBuilder.ts
@@ -0,0 +1,89 @@
+import {
+	type PaginatorComponents,
+	PaginatorSelectMenu,
+	type PaginatorSelectMenuOption
+} from "structures/paginators/components/PaginatorSelectMenu.js";
+import { type ComponentEmojiResolvable } from "discord.js";
+
+export class PaginatorSelectMenuBuilder {
+	private customId: string;
+	private options: PaginatorSelectMenuOption[] = [];
+	private placeholder?: string;
+	private chunkSize?: number;
+	private timeout?: number;
+	private nextPageLabel?: string;
+	private nextPageEmoji: ComponentEmojiResolvable;
+	private previousPageLabel?: string;
+	private previousPageEmoji: ComponentEmojiResolvable;
+
+	setCustomId(customId: string): this {
+		this.customId = customId;
+		return this;
+	}
+
+	setOptions(options: PaginatorSelectMenuOption[]): this {
+		this.options = options;
+		return this;
+	}
+
+	addOption(option: PaginatorSelectMenuOption): this {
+		this.options.push(option);
+		return this;
+	}
+
+	setPlaceholder(placeholder: string): this {
+		this.placeholder = placeholder;
+		return this;
+	}
+
+	setChunkSize(chunkSize: number): this {
+		this.chunkSize = chunkSize;
+		return this;
+	}
+
+	setTimeout(timeout: number): this {
+		this.timeout = timeout * 1000;
+		return this;
+	}
+
+	setNextPageLabel(label: string): this {
+		this.nextPageLabel = label;
+		return this;
+	}
+
+	setNextPageEmoji(emoji: ComponentEmojiResolvable): this {
+		this.nextPageEmoji = emoji;
+		return this;
+	}
+
+	setPreviousPageLabel(label: string): this {
+		this.previousPageLabel = label;
+		return this;
+	}
+
+	setPreviousPageEmoji(emoji: ComponentEmojiResolvable): this {
+		this.previousPageEmoji = emoji;
+		return this;
+	}
+
+	build(): PaginatorComponents {
+		if (!this.customId) {
+			throw new Error("PaginatorSelectMenuBuilder: customId is required.");
+		}
+		if (!this.options.length) {
+			throw new Error("PaginatorSelectMenuBuilder: at least one option is required.");
+		}
+
+		return new PaginatorSelectMenu({
+			customId: this.customId,
+			options: this.options,
+			placeholder: this.placeholder,
+			chunkSize: this.chunkSize,
+			timeout: this.timeout,
+			nextPageLabel: this.nextPageLabel,
+			nextPageEmoji: this.nextPageEmoji,
+			previousPageLabel: this.previousPageLabel,
+			previousPageEmoji: this.previousPageEmoji
+		}).build();
+	}
+}
