f77fe448b1dcff1a18adaccf0503d6233196317f
Tweenty <tweenty.pro@gmail.com>
2026-08-15 12:11:10 +0200

    refactor: new tsconfig/tsdown config

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -3,19 +3,19 @@ import { toError } from "@utils/index.js";
 
 const prefix = "[StelliaJS]";
 export const logger = {
-	info: (message: string) => {
+	info: (message: string): void => {
 		console.log(`${logSymbols.info} ${prefix} ${message}`);
 	},
-	success: (message: string) => {
+	success: (message: string): void => {
 		console.log(`${logSymbols.success} ${prefix} ${message}`);
 	},
-	warn: (message: string) => {
+	warn: (message: string): void => {
 		console.warn(`${logSymbols.warning} ${prefix} ${message}`);
 	},
-	error: (message: string) => {
+	error: (message: string): void => {
 		console.error(`${logSymbols.error} ${prefix} ${message}`);
 	},
-	errorWithInformation: (message: string, error: unknown) => {
+	errorWithInformation: (message: string, error: unknown): void => {
 		console.error(`${logSymbols.error} ${prefix} ${message}:`, toError(error));
 	}
 };
