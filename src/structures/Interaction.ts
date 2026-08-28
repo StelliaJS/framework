9fbb806c030c487df08569a537e1e3a571261edc
Tweentyy <tweenty.pro@gmail.com>
2026-08-28 18:15:27 +0200

    feat: improve handler for every interaction

--- src/structures/Interaction.ts
+++ src/structures/Interaction.ts
@@ -3,8 +3,8 @@ import {
 	type AutocompleteInteraction,
 	type Awaitable,
 	type ButtonInteraction,
-	type ChatInputCommandInteraction,
 	type ContextMenuCommandBuilder,
+	type ChatInputCommandInteraction,
 	type MessageContextMenuCommandInteraction,
 	type ModalSubmitInteraction,
 	type SlashCommandOptionsOnlyBuilder,
@@ -15,89 +15,106 @@ import { type EventStructure } from "structures/Event.js";
 import { type StelliaClient } from "@client/index.js";
 import { type GuildConfigurationType, type StelliaLocale } from "@typescript/index.js";
 
-export interface AutoCompleteStructureWithGuildConfiguration extends Omit<MessageInteractionStructure, "data"> {
+export interface AutoCompleteStructureWithGuildConfiguration<TConfig extends GuildConfigurationType = GuildConfigurationType> extends Omit<
+	MessageInteractionStructure,
+	"data"
+> {
 	data: Omit<MessageDataStructure, "reply">;
 	execute(
-		client: StelliaClient,
-		guildConfiguration: GuildConfigurationType,
+		client: StelliaClient<true>,
+		guildConfiguration: TConfig,
 		memberLocale: StelliaLocale,
 		interaction: AutocompleteInteraction<"cached">
 	): Awaitable<unknown>;
 }
 export interface AutoCompleteStructureWithoutGuildConfiguration extends Omit<MessageInteractionStructure, "data"> {
 	data: Omit<MessageDataStructure, "reply">;
-	execute(client: StelliaClient, memberLocale: StelliaLocale, interaction: AutocompleteInteraction<"cached">): Awaitable<unknown>;
+	execute(client: StelliaClient<true>, memberLocale: StelliaLocale, interaction: AutocompleteInteraction<"cached">): Awaitable<unknown>;
 }
-export type AutoCompleteStructure = AutoCompleteStructureWithGuildConfiguration | AutoCompleteStructureWithoutGuildConfiguration;
+export type AutoCompleteStructure = AutoCompleteStructureWithGuildConfiguration<any> | AutoCompleteStructureWithoutGuildConfiguration;
 
-export interface ButtonStructureWithGuildConfiguration extends MessageInteractionStructure {
+export interface ButtonStructureWithGuildConfiguration<
+	TConfig extends GuildConfigurationType = GuildConfigurationType
+> extends MessageInteractionStructure {
 	execute(
-		client: StelliaClient,
-		guildConfiguration: GuildConfigurationType,
+		client: StelliaClient<true>,
+		guildConfiguration: TConfig,
 		memberLocale: StelliaLocale,
 		interaction: ButtonInteraction<"cached">
 	): Awaitable<unknown>;
 }
 export interface ButtonStructureWithoutGuildConfiguration extends MessageInteractionStructure {
-	execute(client: StelliaClient, memberLocale: StelliaLocale, interaction: ButtonInteraction<"cached">): Awaitable<unknown>;
+	execute(client: StelliaClient<true>, memberLocale: StelliaLocale, interaction: ButtonInteraction<"cached">): Awaitable<unknown>;
 }
-export type ButtonStructure = ButtonStructureWithGuildConfiguration | ButtonStructureWithoutGuildConfiguration;
+export type ButtonStructure = ButtonStructureWithGuildConfiguration<any> | ButtonStructureWithoutGuildConfiguration;
 
-export interface CommandStructureWithGuildConfiguration extends CommandInteractionStructure {
+export interface CommandStructureWithGuildConfiguration<
+	TConfig extends GuildConfigurationType = GuildConfigurationType
+> extends CommandInteractionStructure {
 	execute(
-		client: StelliaClient,
-		guildConfiguration: GuildConfigurationType,
+		client: StelliaClient<true>,
+		guildConfiguration: TConfig,
 		memberLocale: StelliaLocale,
 		interaction: ChatInputCommandInteraction<"cached">
 	): Awaitable<unknown>;
 }
 export interface CommandStructureWithoutGuildConfiguration extends CommandInteractionStructure {
-	execute(client: StelliaClient, memberLocale: StelliaLocale, interaction: ChatInputCommandInteraction<"cached">): Awaitable<unknown>;
-}
-export type CommandStructure = CommandStructureWithGuildConfiguration | CommandStructureWithoutGuildConfiguration;
-
-export interface ContextMenuStructureWithGuildConfiguration extends ContextMenuInteractionStructure {
 	execute(
-		client: StelliaClient,
-		guildConfiguration: GuildConfigurationType,
+		client: StelliaClient<true>,
+		memberLocale: StelliaLocale,
+		interaction: ChatInputCommandInteraction<"cached">
+	): Awaitable<unknown>;
+}
+export type CommandStructure = CommandStructureWithGuildConfiguration<any> | CommandStructureWithoutGuildConfiguration;
+
+export interface ContextMenuStructureWithGuildConfiguration<
+	TConfig extends GuildConfigurationType = GuildConfigurationType
+> extends ContextMenuInteractionStructure {
+	execute(
+		client: StelliaClient<true>,
+		guildConfiguration: TConfig,
 		memberLocale: StelliaLocale,
 		interaction: MessageContextMenuCommandInteraction<"cached"> | UserContextMenuCommandInteraction<"cached">
 	): Awaitable<unknown>;
 }
 export interface ContextMenuStructureWithoutGuildConfiguration extends ContextMenuInteractionStructure {
 	execute(
-		client: StelliaClient,
+		client: StelliaClient<true>,
 		memberLocale: StelliaLocale,
 		interaction: MessageContextMenuCommandInteraction<"cached"> | UserContextMenuCommandInteraction<"cached">
 	): Awaitable<unknown>;
 }
-export type ContextMenuStructure = ContextMenuStructureWithGuildConfiguration | ContextMenuStructureWithoutGuildConfiguration;
+export type ContextMenuStructure = ContextMenuStructureWithGuildConfiguration<any> | ContextMenuStructureWithoutGuildConfiguration;
 
-export interface ModalStructureWithGuildConfiguration extends MessageInteractionStructure {
+export interface ModalStructureWithGuildConfiguration<
+	TConfig extends GuildConfigurationType = GuildConfigurationType
+> extends MessageInteractionStructure {
 	execute(
-		client: StelliaClient,
-		guildConfiguration: GuildConfigurationType,
+		client: StelliaClient<true>,
+		guildConfiguration: TConfig,
 		memberLocale: StelliaLocale,
 		interaction: ModalSubmitInteraction<"cached">
 	): Awaitable<unknown>;
 }
 export interface ModalStructureWithoutGuildConfiguration extends MessageInteractionStructure {
-	execute(client: StelliaClient, memberLocale: StelliaLocale, interaction: ModalSubmitInteraction<"cached">): Awaitable<unknown>;
+	execute(client: StelliaClient<true>, memberLocale: StelliaLocale, interaction: ModalSubmitInteraction<"cached">): Awaitable<unknown>;
 }
-export type ModalStructure = ModalStructureWithGuildConfiguration | ModalStructureWithoutGuildConfiguration;
+export type ModalStructure = ModalStructureWithGuildConfiguration<any> | ModalStructureWithoutGuildConfiguration;
 
-export interface SelectMenuStructureWithGuildConfiguration extends MessageInteractionStructure {
+export interface SelectMenuStructureWithGuildConfiguration<
+	TConfig extends GuildConfigurationType = GuildConfigurationType
+> extends MessageInteractionStructure {
 	execute(
-		client: StelliaClient,
-		guildConfiguration: GuildConfigurationType,
+		client: StelliaClient<true>,
+		guildConfiguration: TConfig,
 		memberLocale: StelliaLocale,
 		interaction: AnySelectMenuInteraction<"cached">
 	): Awaitable<unknown>;
 }
 export interface SelectMenuStructureWithoutGuildConfiguration extends MessageInteractionStructure {
-	execute(client: StelliaClient, memberLocale: StelliaLocale, interaction: AnySelectMenuInteraction<"cached">): Awaitable<unknown>;
+	execute(client: StelliaClient<true>, memberLocale: StelliaLocale, interaction: AnySelectMenuInteraction<"cached">): Awaitable<unknown>;
 }
-export type SelectMenuStructure = SelectMenuStructureWithGuildConfiguration | SelectMenuStructureWithoutGuildConfiguration;
+export type SelectMenuStructure = SelectMenuStructureWithGuildConfiguration<any> | SelectMenuStructureWithoutGuildConfiguration;
 
 export type AnyInteractionStructure =
 	| AutoCompleteStructure
@@ -134,3 +151,57 @@ interface MessageDataStructure {
 }
 
 type ReplyStructure<T extends boolean = false> = T extends true ? { autoDefer: true; ephemeral: boolean } : { autoDefer: false };
+
+export function defineAutoComplete(event: AutoCompleteStructureWithoutGuildConfiguration): AutoCompleteStructureWithoutGuildConfiguration {
+	return event;
+}
+export function createGuildAutoCompleteFactory<TConfig extends GuildConfigurationType = GuildConfigurationType>() {
+	return function (event: AutoCompleteStructureWithGuildConfiguration<TConfig>): AutoCompleteStructureWithGuildConfiguration<TConfig> {
+		return event;
+	};
+}
+
+export function defineButton(event: ButtonStructureWithoutGuildConfiguration): ButtonStructureWithoutGuildConfiguration {
+	return event;
+}
+export function createGuildButtonFactory<TConfig extends GuildConfigurationType = GuildConfigurationType>() {
+	return function (event: ButtonStructureWithGuildConfiguration<TConfig>): ButtonStructureWithGuildConfiguration<TConfig> {
+		return event;
+	};
+}
+
+export function defineCommand(event: CommandStructureWithoutGuildConfiguration): CommandStructureWithoutGuildConfiguration {
+	return event;
+}
+export function createGuildCommandFactory<TConfig extends GuildConfigurationType = GuildConfigurationType>() {
+	return function (event: CommandStructureWithGuildConfiguration<TConfig>): CommandStructureWithGuildConfiguration<TConfig> {
+		return event;
+	};
+}
+
+export function defineContextMenu(event: ContextMenuStructureWithoutGuildConfiguration): ContextMenuStructureWithoutGuildConfiguration {
+	return event;
+}
+export function createGuildContextMenuFactory<TConfig extends GuildConfigurationType = GuildConfigurationType>() {
+	return function (event: ContextMenuStructureWithGuildConfiguration<TConfig>): ContextMenuStructureWithGuildConfiguration<TConfig> {
+		return event;
+	};
+}
+
+export function defineModal(event: ModalStructureWithoutGuildConfiguration): ModalStructureWithoutGuildConfiguration {
+	return event;
+}
+export function createGuildModalFactory<TConfig extends GuildConfigurationType = GuildConfigurationType>() {
+	return function (event: ModalStructureWithGuildConfiguration<TConfig>): ModalStructureWithGuildConfiguration<TConfig> {
+		return event;
+	};
+}
+
+export function defineSelectMenu(event: SelectMenuStructureWithoutGuildConfiguration): SelectMenuStructureWithoutGuildConfiguration {
+	return event;
+}
+export function createGuildSelectMenuFactory<TConfig extends GuildConfigurationType = GuildConfigurationType>() {
+	return function (event: SelectMenuStructureWithGuildConfiguration<TConfig>): SelectMenuStructureWithGuildConfiguration<TConfig> {
+		return event;
+	};
+}
